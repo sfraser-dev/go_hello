847b41efc91449a9780530bebf46fee74d49b97f
Stew Fraser <stewart.fraser@beatzero.co.uk>
2024-09-07 11:11:40 +0100

    adding terminal output info

--- hello.go
+++ hello.go
@@ -18,29 +18,28 @@ func main() {
 		nada4 bool
 	)
 	e, f := "oops", false
-
 	// snippet "fmp" "fmpl"
-	fmt.Println("Hi", myName)
-	fmt.Print("Hi ", myName, "\n")
-	fmt.Println("pi = ", PI)
-	fmt.Println(a, b, c, d)
-	fmt.Println(nada1, nada2, nada3, nada4)
-	fmt.Println(e, f)
+	fmt.Println("Hi", myName)               // Joe Bloggs
+	fmt.Print("Hi ", myName, "\n")          // Joe Bloggs
+	fmt.Println("pi = ", PI)                // pi =  3.14159265358
+	fmt.Println(a, b, c, d)                 // mars 42 true 21
+	fmt.Println(nada1, nada2, nada3, nada4) // 0 0 false
+	fmt.Println(e, f)                       // oops false
 	// printf formatting verbs (type and value)
 	fmt.Printf("b is of type %T and has value %v\n", b, b)
-	fmt.Printf("%d\n", d)       // %d ints
-	fmt.Printf("%+d\n", d)      // always show sign
-	fmt.Printf("%4dOops\n", d)  // right justified by 4
-	fmt.Printf("%-4dOops\n", d) // left justified by 4
+	fmt.Printf("%d\n", d)       // %d int
+	fmt.Printf("%4dOops\n", d)  //    int right justified by 4
+	fmt.Printf("%-4dOops\n", d) //    int left justified by 4
+	fmt.Printf("%+d\n", d)      //    int always show sign
 	fmt.Printf("%s\n", a)       // %s string
-	fmt.Printf("%q\n", a)       // double Quoted string
-	fmt.Printf("%8sOops\n", a)  // right justified
-	fmt.Printf("%-8sOops\n", a) // left justified
+	fmt.Printf("%q\n", a)       // 	  string double quoted
+	fmt.Printf("%8sOops\n", a)  // 	  string right justified
+	fmt.Printf("%-8sOops\n", a) // 	  string left justified
 	fmt.Printf("%f\n", PI)      // %f floats
-	fmt.Printf("%.2f\n", PI)    // precision 2
-	fmt.Printf("%4.2f\n", PI)   // precision 2 width 4
+	fmt.Printf("%.2f\n", PI)    //    float precision 2
+	fmt.Printf("%4.2f\n", PI)   // 	  float precision 2 width 4
 
-	// value formatting (prints out anything, even arrays and slices)
+	// value (%v) formatting prints out anything, even arrays and slices
 	var myArr = [5]int{1000, 1001, 1002, 1003, 1004}
 	fmt.Printf("int is: %v, string is: %v, float is %v, array is %v\n", 100, "hello, world!", 3.14, myArr)
 
@@ -56,36 +55,37 @@ func main() {
 	// array
 	var nos = [4]uint{101, 201, 301, 401}
 	var drinks = [4]string{"coke", "pepsi", "sprite", "fanta"}
+	// size of array will be auto determined based on number of elements provided during initialization
 	var cars = [...]string{"bmw", "ford", "peugeot", "ferrari"}
 	flts := [...]float32{1.1, 2.2, 3.3, 4}
-	fmt.Println(nos)
-	fmt.Println(drinks)
-	fmt.Println("cars[0] = ", cars[0])
-	fmt.Println(flts)
-	fmt.Println("len of array cars is:", len(cars))
+	fmt.Println(nos)								// [101 201 301 401]	
+	fmt.Println(drinks)							    // [coke pepsi sprite fanta]
+	fmt.Println("cars[0] = ", cars[0])				// cars[0] =  bmw
+	fmt.Println(flts)								// [1.1 2.2 3.3 4]
+	fmt.Println("len of array cars is:", len(cars)) // len of array cars is: 4
 
 	// empty slice (dynamic)
 	sl1 := []int{}
-	fmt.Println("slice len is", len(sl1))
-	/* capacity of underlying array in the slice
+	fmt.Println("slice len is", len(sl1))		// slice len is 0
+	/* capacity ("cap") of underlying array in the slice
 	   if more underlying array elements needed, Go will copy
 	   to a new underlying array with (usually) twice the capacity */
-	fmt.Println("slice cap is", cap(sl1))
-	fmt.Println(sl1)
+	fmt.Println("slice cap is", cap(sl1))		// slice cap is 0
+	fmt.Println(sl1)							// []	
 	// non-empty slice (dynamic)
 	sl2 := []string{"Go", "Slices", "Are", "Powerful"}
-	fmt.Println("slice len is", len(sl2))
-	fmt.Println("slice cap is", cap(sl2))
-	fmt.Println(sl2)
+	fmt.Println("slice len is", len(sl2))		// slice len is 4
+	fmt.Println("slice cap is", cap(sl2))		// slice cap is 4
+	fmt.Println(sl2)							// [Go Slices Are Powerful]	
 	sl2 = append(sl2, "Growwwwing")
-	fmt.Println("slice len is", len(sl2))
-	fmt.Println("slice cap is", cap(sl2))
-	fmt.Println(sl2)
+	fmt.Println("slice len is", len(sl2))		// slice len is 5
+	fmt.Println("slice cap is", cap(sl2))		// slice cap is 8
+	fmt.Println(sl2)							// [Go Slices Are Powerful Growwwwing]
 	// create slice from array
 	testArr := [3]int{1000, 1001, 1002}
 	testSlice := testArr[0:2]
-	fmt.Println("len testSlice", len(testSlice))
-	fmt.Println("cap testSlice", cap(testSlice))
-	fmt.Println(testSlice)
-	fmt.Printf("testSlice: %v\n", testSlice)
+	fmt.Println("len testSlice", len(testSlice))	// len testSlice 2
+	fmt.Println("cap testSlice", cap(testSlice))	// cap testSlice 3
+	fmt.Println(testSlice)							// [1000 1001]
+	fmt.Printf("testSlice: %v\n", testSlice)		// testSlice: [1000 1001]
 }
